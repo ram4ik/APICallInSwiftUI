d2dc0102e985b556f33f8a44b25ce405b0fd899f
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-07-29 22:33:26 +0300

    init

--- APICallInSwiftUI/ContentView.swift
+++ APICallInSwiftUI/ContentView.swift
@@ -7,9 +7,48 @@
 
 import SwiftUI
 
+struct Result: Codable {
+    let trackId: Int
+    let trackName: String
+    let collectionName: String
+}
+
+struct Response: Codable {
+    let results: [Result]
+}
+
 struct ContentView: View {
+    @State private var results = [Result]()
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        NavigationView {
+            List(results, id: \.trackId) { item in
+                VStack(alignment: .leading) {
+                    Text(item.trackName)
+                        .font(.headline)
+                    
+                    Text(item.collectionName)
+                }
+            }.onAppear(perform: loadData)
+            .navigationBarTitle("iTunes API")
+        }
+    }
+    
+    func loadData() {
+        guard let url = URL(string: "http://itunes.apple.com/search?term=taylor+swift&entity=song") else { return }
+        
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                    DispatchQueue.main.async {
+                        self.results = decodedResponse.results
+                    }
+                    return
+                }
+            }
+        }.resume()
     }
 }
 
