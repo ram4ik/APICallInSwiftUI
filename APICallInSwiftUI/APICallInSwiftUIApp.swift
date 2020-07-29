06c5fb50d16bd991fdf925d5613196b2689df86c
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-07-29 22:16:09 +0300

    Initial Commit

---
+++ APICallInSwiftUI/APICallInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  APICallInSwiftUIApp.swift
+//  APICallInSwiftUI
+//
+//  Created by Ramill Ibragimov on 29.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct APICallInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
